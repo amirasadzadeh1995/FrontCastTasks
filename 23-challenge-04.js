679602b02c1b1d527f87331e234bd8787e3a1595
AmirAsadzadeh1995 <amirasadzadeh1995@gmail.com>
2023-04-15 12:53:00 +0330

    23

--- 23-challenge-04.js
+++ 23-challenge-04.js
@@ -1,1 +1,23 @@
-hello
+const ProductItems = [{
+    title: `book1`
+}, {
+    title: `book2`
+}, {
+    title: `book3`
+}, {
+    title: `book4`
+}]
+
+const deletProduct = function(products,productTitle){
+   const indexValue = products.findIndex(function(item){
+    return item.title.toLowerCase() === productTitle
+   })
+   if(indexValue > -1){
+    products.splice(indexValue, 1)
+   } else {
+    console.log(`Product Not Found`)
+   }
+}
+
+deletProduct(ProductItems, `book2`)
+console.log(ProductItems)
