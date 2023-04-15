efce37d5bfcbcfbb5d4678536c2f6cfa1254edc1
AmirAsadzadeh1995 <amirasadzadeh1995@gmail.com>
2023-04-15 14:23:46 +0330

    24

---
+++ 24-1-filter-method.js
@@ -0,0 +1,6 @@
+const numbers = [12, 22, 9, 3, 2]
+const filteredNumbers = numbers.filter(function(item) {
+return item >= 10
+})
+
+console.log(filteredNumbers)
---
+++ 24-2-filter-method.js
@@ -0,0 +1,24 @@
+const productItems = [{
+    title: `book1`,
+    exist:true
+},{
+    title: `book2`,
+    exist:false
+},{
+    title: `book3`,
+    exist:true
+},{
+    title: `book4`,
+    exist:false
+},{
+}]
+
+const productNotExist = function(products) {
+    return products.filter(function(item){
+    return !item.exist 
+} )
+}
+
+let Resault = productNotExist(productItems)
+
+console.log(Resault)
