ecec947ffcaca7c2ce0c6f8023c3762f4d62cf66
AmirAsadzadeh1995 <amirasadzadeh1995@gmail.com>
2023-04-15 16:17:18 +0330

    25

---
+++ 25-sort-method.js
@@ -0,0 +1,31 @@
+const productItems = [{
+    title: `book1`,
+    exist:true
+},{
+    title: `book2`,
+    exist:false
+},{
+    title: `book3`,
+    exist:true
+},{
+    title: `book4`,
+    exist:false
+},{
+}]
+
+const sortProducts = function(products){
+    products.sort(function(a,b){
+if(a.exist === true && b.exist === false){
+        return -1;
+} else if(b.exist === true && a.exist === false){
+    return 1
+} else {
+    return 0
+}
+    })
+}
+
+
+
+sortProducts(productItems)
+console.log(productItems)
