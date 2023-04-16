f66bbaf80ed4c4488ef193da30a699ac13071ec5
AmirAsadzadeh1995 <amirasadzadeh1995@gmail.com>
2023-04-16 18:00:33 +0330

    37

--- 37/app.js
+++ 37/app.js
@@ -1,31 +1,5 @@
-const products = [{
-    title: 'Node. is Design Patterns'
-},{
-    title: 'You Dont Know JS: this & Object Prototypes'
-},{
-    title: 'Functional React'
-},{
-    title: 'You Dont Know JS: Async & Performance'
-}]
-
-const filters ={
-    searchItem: ''
-}
-
-const renderProducts = function(products,filters){
-    const filteredProducts = products.filter(function(item){
-        return item.title.toLowerCase().includes(filters.searchItem.toLowerCase())
-    })
-    document.querySelector('#products').innerHTML = ''
-    filteredProducts.forEach(function(item){
-        const productEl = document.createElement('p')
-        productEl.textContent = item.title
-        document.querySelector('#products').appendChild(productEl)
-    })
-}
-
-renderProducts(products,filters)
-document.querySelector('#search-products').addEventListener('input',function(e){
-    filters.searchItem = e.target.value
-    renderProducts(products,filters)
+document.querySelector('#add-Product-Form').addEventListener('submit',function(e){
+    e.preventDefault()
+    console.log(e.target.elements.productTitle.value)    
+    e.target.elements.productTitle.value = ''
 })
