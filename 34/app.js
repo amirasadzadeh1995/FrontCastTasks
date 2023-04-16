b3071ec19cf5f9e0af2153dd5cf51ffd60ca4a62
AmirAsadzadeh1995 <amirasadzadeh1995@gmail.com>
2023-04-16 16:12:58 +0330

    34

---
+++ 34/app.js
@@ -0,0 +1,9 @@
+document.querySelector('#add-product').addEventListener('click', function(e){
+console.log('product created')
+})
+
+document.querySelector('#remove-all-products').addEventListener('click', function(e){
+    document.querySelectorAll('.product').forEach(function(item){
+        item.remove()
+    })
+    })
