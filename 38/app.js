d2fea6d72cc28f3af03f52e659af842820870634
AmirAsadzadeh1995 <amirasadzadeh1995@gmail.com>
2023-04-16 20:28:19 +0330

    39

---
+++ 38/app.js
@@ -0,0 +1,5 @@
+document.querySelector('#add-Product-Form').addEventListener('submit',function(e){
+    e.preventDefault()
+    console.log(e.target.elements.productTitle.value)    
+    e.target.elements.productTitle.value = ''
+})
