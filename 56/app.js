93199889cbfa0542928962b12250ce8d028df33a
AmirAsadzadeh1995 <amirasadzadeh1995@gmail.com>
2023-04-26 15:22:10 +0330

    56

---
+++ 56/app.js
@@ -0,0 +1,49 @@
+let products = getSaveProducts();
+
+const filters = {
+  searchItem: "",
+  availableProducts: false,
+};
+
+renderProducts(products, filters);
+
+document
+  .querySelector("#search-products")
+  .addEventListener("input", function (e) {
+    filters.searchItem = e.target.value;
+    renderProducts(products, filters);
+  });
+
+document
+  .querySelector("#add-product-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    const id = uuidv4();
+    products.push({
+      id: id,
+      title: e.target.elements.productTitle.value,
+      price: "",
+      exist: true,
+    });
+    saveProducts(products);
+    renderProducts(products, filters);
+    e.target.elements.productTitle.value = "";
+  });
+
+document
+  .querySelector("#available-products")
+  .addEventListener("change", function (e) {
+    filters.availableProducts = e.target.checked;
+    renderProducts(products, filters);
+  });
+window.addEventListener(`storage`, function (e) {
+  if (e.key === `products`) {
+    products = JSON.parse(e.newValue);
+    renderProducts(products, filters);
+  }
+});
+const now1 = moment();
+
+// now1.add(1, `year`).subtract(100, `year`);
+// console.log(now1.toString());
+console.log(now1.format(`MMMM Do, YYYY`));
