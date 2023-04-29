bd1a05c439eb17dbcce41ebe894ad1bed2f87944
AmirAsadzadeh1995 <amirasadzadeh1995@gmail.com>
2023-04-29 16:32:26 +0330

    64

---
+++ 64/app.js
@@ -0,0 +1,10 @@
+const price = 777;
+
+// if (price > 59) {
+//   message = `Has Discount!`;
+// } else {
+//   message = `Has Not Discount `;
+// }
+const message = price > 59 ? `Has Discount` : `Has Not Discount`;
+
+console.log(message);
