4c2a46fa78f41b4e0a61cf8c94d6940997cc44ad
AmirAsadzadeh1995 <amirasadzadeh1995@gmail.com>
2023-05-08 16:25:00 +0330

    80

---
+++ 80/app.js
@@ -0,0 +1,31 @@
+const getBtn = document.querySelector(`#get-btn`);
+const postBtn = document.querySelector(`#post-btn`);
+
+const sendHttpRequest = (method, url) => {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = `json`;
+    xhr.onload = () => {
+      resolve(xhr.response);
+    };
+    xhr.onerror = () => {
+      reject(`Error`);
+    };
+    xhr.send();
+  });
+  return promise;
+};
+
+const getData = () => {
+  sendHttpRequest(`GET`, `https://jsonplaceholder.typicode.com/posts/1`);
+  then((res) => {
+    console.log(res);
+  }).catch((err) => {
+    console.log(err);
+  });
+};
+
+const postData = () => {};
+getBtn.addEventListener(`click`, getData);
+postBtn.addEventListener(`click`, postData);
