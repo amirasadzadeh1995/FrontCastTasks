a71db22deab4e27e75ea00b2a0d70fab5d449a1f
AmirAsadzadeh1995 <amirasadzadeh1995@gmail.com>
2023-05-23 11:03:51 +0330

    webpack-project

---
+++ webpack-project/src/index.js
@@ -0,0 +1,5 @@
+import id, { productTitle, productPrice } from './util'
+
+console.log(productTitle('Book'))
+console.log(productPrice(79))
+console.log(id(5))
---
+++ webpack-project/src/util.js
@@ -0,0 +1,7 @@
+const productTitle = title => title
+
+const productPrice = price => price
+
+const productId = id => id
+
+export { productTitle, productPrice, productId as default }
